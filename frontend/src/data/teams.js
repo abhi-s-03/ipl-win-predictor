a5c9343bd5847aa11d510bded2afeda45d0fa327
abhi-s-03 <abhisheks3002@gmail.com>
2024-12-06 23:17:03 +0530

    data added

---
+++ frontend/src/data/teams.js
@@ -0,0 +1,42 @@
+export const teams = [
+  {
+    id: 'csk',
+    name: 'Chennai Super Kings',
+  },
+  {
+    id: 'mi',
+    name: 'Mumbai Indians',
+  },
+  {
+    id: 'rcb',
+    name: 'Royal Challengers Bangalore',
+  },
+  {
+    id: 'kkr',
+    name: 'Kolkata Knight Riders',
+  },
+  {
+    id: 'dc',
+    name: 'Delhi Capitals',
+  },
+  {
+    id: 'rr',
+    name: 'Rajasthan Royals',
+  },
+  {
+    id: 'pbks',
+    name: 'Punjab Kings',
+  },
+  {
+    id: 'srh',
+    name: 'Sunrisers Hyderabad',
+  },
+  {
+    id: 'gt',
+    name: 'Gujarat Titans',
+  },
+  {
+    id: 'lsg',
+    name: 'Lucknow Super Giants',
+  },
+];
